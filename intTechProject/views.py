d6b570c74061ab30c0470cf353ea07a6a08659c5
Blair <blaircalderwood@live.co.uk>
2016-02-25 18:58:33 +0000

    Started work on populationScript and changed ordering in models

--- intTechProject/views.py
+++ intTechProject/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from mainapp.models import User
 
+
 def base_profile(request):
     return render(request, 'base.html')
 
 
 def index(request):
-	user_list = User.objects.all()
-	context_dict = {"users" : user_list}
+    user_list = User.objects.all()
+    context_dict = {"users": user_list}
 
-	return render(request, "index.html", context_dict)
-
+    return render(request, "index.html", context_dict)
--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from mainapp.models import User
+from mainapp.models import City, User, Hobby, Language, UserRating
 
+
+admin.site.register(City)
 admin.site.register(User)
+admin.site.register(Hobby)
+admin.site.register(Language)
+admin.site.register(UserRating)
+
