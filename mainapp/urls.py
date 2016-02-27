30270aeb228db70d6fee146f3db40f55a7db3338
Martin Grant <martingrant@outlook.com>
2016-02-27 13:33:16 +0000

    Added a urls file in /mainapp/, the original url file includes the urls from the new file. This is to match the structure of tangowithdjango.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, patterns
 from django.contrib import admin
 from intTechProject import views
 
 urlpatterns = patterns('',
-    url(r'^', include('mainapp.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name="index"),
+    url(r'^baseProfile/', views.base_profile, name="baseProfile"),
+    url(r'^(?P<city_name_slug>\w+)$', views.city, name='city'),
     )
