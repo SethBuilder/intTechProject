690a7ec107896f16c96e376a1fe114f63eae6fc8
Martin Grant <martingrant@outlook.com>
2016-03-16 14:51:09 +0000

    Searching for a city displays the city name as a link to the city page.

--- intTechProject/views.py
+++ intTechProject/views.py
@@ -94,9 +94,9 @@ def search_form(request):
 def search(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        books = City.objects.filter(name__icontains=q)
+        cities = City.objects.filter(name__icontains=q)
         return render(request, 'search_results.html',
-            {'books': books, 'query': q})
+            {'cities': cities, 'query': q})
     else:
         return HttpResponse('Please submit a search term.')  
     
