8b5926e3022975fd608f9809c199a4fec5823d12
Martin Grant <martingrant@outlook.com>
2016-03-17 12:42:17 +0000

    Removed search button on nav bar. Search by either using the city search on main index page, or the search input box on the nav bar. Searching either way will bring the user to the same results page, where they have another bar that they can use to search without leaving the page. Removed search form template and view.

--- intTechProject/urls.py
+++ intTechProject/urls.py
@@ -33,8 +33,6 @@ urlpatterns = patterns('',
     url(r'^city/(?P<city_name_slug>[\w-]+)$', views.city, name='city'),
     url(r'^user/(?P<user_name_slug>\w+)$', views.user, name='user'),
     url(r'^', include('registration.backends.simple.urls')),
-    
     url(r'^createprofile/', views.createprofile, name="createprofile"),
-    
-    
-    url(r'^search/$', views.search, name="search"), )
+    url(r'^search/$', views.search, name="search"), 
+    )
--- intTechProject/views.py
+++ intTechProject/views.py
@@ -15,11 +15,6 @@ from django.db.models import Q
 
 def index(request):
     user_list = UserProfile.objects.select_related().annotate(rating=Avg('rated_user__rating')).order_by('-rating')[:5]
-
-    # to round ratings
-    # for user in user_list:
-    #    user.rating = int(user.rating)
-
     city_list = City.objects.select_related().annotate(total=Count('userprofile__id')).order_by('-total')[:5]
 
     context_dict = {"users": user_list, "cities": city_list,}
@@ -33,28 +28,13 @@ def index(request):
         else:
             try:
                 cities = City.objects.filter(name__icontains=q)
-                return city(request, q)
-            except:
-                return render(request, 'search_results_cities.html', {'cities': cities, 'query': q})
-
-    else:
-        q2 = request.GET.get('q2')
-        if not q2:
-            error = True
-        else:
-            try:
-                cities = City.objects.filter(name__icontains=q2)
-                users = User.objects.filter(Q(username__icontains=q2) | Q(profile__slug__icontains=q2) | Q(first_name__icontains=q2) | Q(last_name__icontains=q2))
+                users = User.objects.filter(Q(username__icontains=q) | Q(profile__slug__icontains=q) | Q(first_name__icontains=q) | Q(last_name__icontains=q))
                 #return city(request, q)
-                return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q2})
+                return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q})
             except:
-                return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q2})
+                return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q})
 
     return render(request, "index.html", context_dict)
-
-
-
-
 
 
 def city(request, city_name_slug):
@@ -116,11 +96,6 @@ def user(request, user_name_slug):
     # Go render the response and return it to the client.
     return render(request, 'profilePage.html', context_dict)
 
-
-def search_form(request):
-    return render(request, 'search_form.html')
-
-
 def search(request):
     error = False
     if 'q' in request.GET:
@@ -136,8 +111,9 @@ def search(request):
             except:
                 return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q})
 
-    return render(request, 'search_form.html',
+    return render(request, 'search_results.html',
         {'error': error})
-    
+   
+
 def createprofile(request):
     return render(request, 'createprofile.html')
