f6d638b1fcfaaa6ee5343d468af9a8fd86d3575f
Martin Grant <martingrant@outlook.com>
2016-03-17 15:32:14 +0000

    Deleted user.html template as it is no longer used.

--- intTechProject/views.py
+++ intTechProject/views.py
@@ -127,7 +127,7 @@ def search(request):
             try:
                 cities = City.objects.filter(name__icontains=q)
                 users = User.objects.filter(Q(username__icontains=q) | Q(profile__slug__icontains=q) | Q(first_name__icontains=q) | Q(last_name__icontains=q))
-                #return city(request, q)
+                
                 return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q})
             except:
                 return render(request, 'search_results.html', {'cities': cities, 'users': users, 'query': q})
