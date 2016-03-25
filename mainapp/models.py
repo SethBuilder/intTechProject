cc19b3223e418776054032e0b58269ea03439f03
Seif Elmughrabi <moghrabi@gmail.com>
2016-03-25 19:21:26 +0000

    commented models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,12 +8,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # this is the model for city
 class City(models.Model):
 
-    name = models.CharField(max_length=128, default="", unique=True)
-    country = models.CharField(max_length=128, default="Scotland")
-    information = models.CharField(max_length=3000, default="")
-    image = models.ImageField(upload_to='city_images', default=0)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=128, default="", unique=True)#city name
+    country = models.CharField(max_length=128, default="Scotland")#country name
+    information = models.CharField(max_length=3000, default="")#information about city
+    image = models.ImageField(upload_to='city_images', default=0)#city image
+    slug = models.SlugField(unique=True)#city slug
 
+    #save function
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(City, self).save(*args, **kwargs)
@@ -22,13 +23,12 @@ class City(models.Model):
         return self.name
 
 
-# this is the model for hobbies - one to many relationship with User
+# this is the model for hobbies
 class Hobby(models.Model):
-    hobby = models.CharField(max_length=128)
+    hobby = models.CharField(max_length=128)#hobby name
 
+    #save function
     def save(self, *args, **kwargs):
-        # if self.id is None:
-        # self.slug = slugify(self.name)
         self.slug = slugify(self.hobby)
         super(Hobby, self).save(*args, **kwargs)
 
@@ -36,13 +36,12 @@ class Hobby(models.Model):
         return self.hobby
 
 
-# this is the model for languages - one to many relationship with User
+# this is the model for languages
 class Language(models.Model):
-    language = models.CharField(max_length=128)
+    language = models.CharField(max_length=128)#language name
 
+    #save function
     def save(self, *args, **kwargs):
-        # if self.id is None:
-        # self.slug = slugify(self.name)
         self.slug = slugify(self.language)
         super(Language, self).save(*args, **kwargs)
 
@@ -52,18 +51,19 @@ class Language(models.Model):
 
 # this is model for user
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='profile')
+    user = models.OneToOneField(User, related_name='profile')#each user profile is associated with one user
 
-    hobbies = models.ManyToManyField(Hobby, blank=True)
-    languages = models.ManyToManyField(Language, blank=True)
+    hobbies = models.ManyToManyField(Hobby, blank=True)#m2m relationship between profiles and hobbies
+    languages = models.ManyToManyField(Language, blank=True)#m2m relationship between profiles and languages
 
-    profilepic = models.ImageField(upload_to='profile_pictures', blank=True)
-    city = models.ForeignKey(City)
-    slug = models.SlugField(unique=True)
+    profilepic = models.ImageField(upload_to='profile_pictures', blank=True)#optinal profile pic
+    city = models.ForeignKey(City)#each profile is associated with a city
+    slug = models.SlugField(unique=True)#slug for profile
 
-    average_rating = models.IntegerField(default=0)
-    ratings_count = models.IntegerField(default=0)
+    average_rating = models.IntegerField(default=0)#avg rating for user
+    ratings_count = models.IntegerField(default=0)#number of ratings user received
 
+    #save function
     def save(self, *args, **kwargs):
 
         # Uncomment if you don't want the slug to change every time the name changes
@@ -73,6 +73,7 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return self.user.username
 
+    #updates avg rating
     def update_average_rating(self, new_rating, *args, **kwargs):
 
         old_total = self.average_rating * self.ratings_count
@@ -89,14 +90,14 @@ class UserProfile(models.Model):
         return range(self.average_rating)
 
 
-# this is the model for user ratings - one to many relationship with User
+# this is the model for user ratings
 class UserRating(models.Model):
-    user = models.ForeignKey(UserProfile, related_name="rated_user")
-    rating_user = models.ForeignKey(UserProfile, related_name="rating_user")
-    comment = models.CharField(max_length=500, blank=True)
-    rating = models.IntegerField(default=0,validators=[MaxValueValidator(5), MinValueValidator(0)
+    user = models.ForeignKey(UserProfile, related_name="rated_user")#one user can have many ratings
+    rating_user = models.ForeignKey(UserProfile, related_name="rating_user")#one user can have many rating users
+    comment = models.CharField(max_length=500, blank=True)#comment
+    rating = models.IntegerField(default=0,validators=[MaxValueValidator(5), MinValueValidator(0)#rating out of 5
         ])
-
+    #save function
     def save(self, *args, **kwargs):
         self.user.update_average_rating(self.rating)
         super(UserRating, self).save(*args, **kwargs)
